410d39d7a12159560bb9d9dca789eefc6df0020a
Maksim Novikov <134298182+fxdfh2@users.noreply.github.com>
2023-08-28 00:57:09 +0300

    Закончил

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector('.modal-wrapper');
 const servicesLinkRare = document.querySelector('.services-link-rare');
 const modalCloseButton = document.querySelector('.modal-close-button');
+const bullitList = document.querySelector('.bulits-list');
+const bullits = document.querySelectorAll('.bulit-button');
 
 servicesLinkRare.addEventListener('click', (evt) => {
   evt.preventDefault();
@@ -10,4 +12,15 @@ servicesLinkRare.addEventListener('click', (evt) => {
 modalCloseButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   modal.classList.remove('modal-show');
+});
+
+
+bullitList.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('bulitButton')) {
+    const activeBullit = evt.target;
+    const indexCurrentBullit = Array.from(bullits).indexOf(activeBullit);
+    setActiveScreen(indexCurrentBullit);
+    shiftSlider(indexCurrentBullit);
+    setActiveBullet(indexCurrentBullit);
+  }
 });
